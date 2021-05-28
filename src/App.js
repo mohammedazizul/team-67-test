2c9676d00f62a89332b6aa6d5f9d80ffb0c0e6b1
appsmakerbd <engrajibulhasan@gmail.com>
2021-05-28 22:54:26 +0600

    I have Added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Home/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
 
 const Header = () => {
     return (
-        <div className="container bg-primary">
-            <h1>This is Header By Rajibul Hasan</h1>
-            <p>Thank you AZIZ vai </p>
-        </div>
+        <section>
+            <Nav/> I am inside header added by azizul
+        </section>
     );
 };
 
