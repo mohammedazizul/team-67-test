b3e68909739dac78e6e0785b0ec563719ace5253
mohammedazizul <mohammedazizulhoquesheikh@gmail.com>
2021-05-28 21:03:08 +0800

    home component added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>HOME</h1>
+            <p>by azizul</p>
+        </div>
+    );
+};
+
+export default Home;
